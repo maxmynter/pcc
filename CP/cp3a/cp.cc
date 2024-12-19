7697932008be38566d5eec9fd88e3e92d53b6aba
Max Mynter <maxmynter@me.com>
2024-12-20 00:22:56 +0100

    CP3 4/5 Points

--- CP/cp3a/cp.cc
+++ CP/cp3a/cp.cc
@@ -3,22 +3,11 @@
 #include <vector>
 using namespace std;
 
-/*
-This is the function you need to implement. Quick reference:
-- input rows: 0 <= y < ny
-- input columns: 0 <= x < nx
-- element at row y and column x is stored in data[x + y*nx]
-- correlation between rows i and row j has to be stored in result[i + j*ny]
-- only parts with 0 <= j <= i < ny need to be filled
-*/
-
-// straigth copy from course material
 typedef double double4_t __attribute__((vector_size(4 * sizeof(double))));
 
 constexpr float param_zero = 0.0;
 constexpr double4_t d4_init{param_zero, param_zero, param_zero, param_zero};
 
-// straigth copy from course material
 static double4_t *double4_alloc(std::size_t n) {
   void *tmp = 0;
   if (posix_memalign(&tmp, sizeof(double4_t), sizeof(double4_t) * n)) {
@@ -36,57 +25,49 @@ static inline double sum_all4(double4_t vv) {
 }
 
 void correlate(const int ny, int nx, const float *data, float *result) {
-  // block size nd
   constexpr int nd = 8;
-  // how many blocks of rows
   int nc = (ny + nd - 1) / nd;
-  // number of rows after padding
   int ncd = nc * nd;
 
-  // elements per vector
   constexpr int nb = 4;
-  // vectors per input row
   const int na = (nx + nb - 1) / nb;
 
-  // input data, padded, converted to vectors
   double4_t *vd = double4_alloc(ncd * na);
   std::vector<double> data_norm(nx * ny);
 
 #pragma omp parallel for schedule(dynamic, 1)
   for (int y = 0; y < ny; ++y) {
-    double sum = 0.0;
-    double mean = 0.0;
-    double pow_sum = 0.0;
-    double sqrt_sum_sqrt = 0.0;
+    const float *const curr_data = data + y * nx;
+    double *const curr_norm = &data_norm[y * nx];
 
+    double sum = 0.0;
     for (int x = nx; x--;) {
-      sum += data[x + y * nx];
+      sum += curr_data[x];
     }
 
-    mean = sum / nx;
+    const double mean = sum / nx;
+    double pow_sum = 0.0;
 
     for (int x = nx; x--;) {
-      double normalized = data[x + y * nx] - mean;
-      data_norm[x + y * nx] = normalized;
+      double normalized = curr_data[x] - mean;
+      curr_norm[x] = normalized;
       pow_sum += normalized * normalized;
     }
 
-    sqrt_sum_sqrt = sqrt(pow_sum);
+    const double inv_sqrt = 1.0 / sqrt(pow_sum);
 
     for (int x = nx; x--;) {
-      data_norm[x + y * nx] /= sqrt_sum_sqrt;
+      curr_norm[x] *= inv_sqrt;
     }
 
-    // NEW
     for (int ka = 0; ka < na; ++ka) {
       for (int kb = 0; kb < nb; kb++) {
         int i = ka * nb + kb;
-        vd[ka + na * y][kb] = i < nx ? data_norm[nx * y + i] : 0.0;
+        vd[ka + na * y][kb] = i < nx ? curr_norm[i] : 0.0;
       }
     }
   }
 
-// see Chapter 2: Case study
 #pragma omp parallel for schedule(dynamic, 1)
   for (int row0 = 0; row0 < ncd - 7; row0 += 8) {
     int row1 = row0 + 1;
@@ -103,12 +84,10 @@ void correlate(const int ny, int nx, const float *data, float *result) {
       int row12 = row8 + 4;
       int row13 = row8 + 5;
       int row14 = row8 + 6;
-      int row15 = row8 + 6;
       int row16 = row8 + 7;
 
       double4_t vv[nd][nd];
 
-      // init
       for (int id = 0; id < nd; ++id) {
         for (int jd = 0; jd < nd; ++jd) {
           vv[id][jd] = d4_init;
